53dcce1fc67b19b70573546264235288e2fd172a
waveboard25 <yug3987@gmail.com>
2026-06-17 17:24:56 +0530

    added the 4 functions into the main

---
+++ main.py
@@ -0,0 +1,19 @@
+def add():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("Addition =", a + b)
+
+def sub():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("Subtraction =", a - b)
+
+def mul():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("Multiplication =", a * b)
+
+def div():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("Division =", a / b)
